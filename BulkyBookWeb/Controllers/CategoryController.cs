2c55d861c8e4681b4db5039508982f5c5c734a48
olcay <olcaytapan10@gmail.com>
2023-07-25 15:47:40 +0200

    Add project files.

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,151 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            //var objCategoryList = _db.Categories.ToList();
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name!");
+            }
+
+            foreach (var item in _db.Categories)
+            {
+				if (obj.Name == item.Name)
+                {
+					ModelState.AddModelError("name", "The Name already exists!");
+				}
+			}
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+		//GET
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			var categoryFromDb = _db.Categories.Find(id);
+			//var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
+			//var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+
+			return View(categoryFromDb);
+		}
+
+		//POST
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(Category obj)
+		{
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+			}
+
+            foreach (var item in _db.Categories)
+            {
+                if (obj.Name == item.Name && obj.Id != item.Id)
+                {
+                    ModelState.AddModelError("name", "The Name already exists!");
+                }
+            }
+
+            if (ModelState.IsValid)
+			{
+				var existingCategory = _db.Categories.Find(obj.Id); // Id'ye göre nesneyi çek
+				if (existingCategory != null)
+				{
+					existingCategory.Name = obj.Name;
+					existingCategory.DisplayOrder = obj.DisplayOrder;
+
+					_db.SaveChanges();
+					TempData["success"] = "Category updated successfully";
+					return RedirectToAction("Index");
+				}
+				else
+				{
+					ModelState.AddModelError("name", "Category not found!"); // Eğer nesne yoksa hata ekle
+				}
+			}
+			return View(obj);
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			var categoryFromDb = _db.Categories.Find(id);
+			//var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
+			//var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+
+			return View(categoryFromDb);
+		}
+
+		//POST
+		//[HttpPost, ActionName("Delete")] // If you want to use this, you must change the name of the method to Delete at the Delete.cshtml page
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult DeletePOST(int? id)
+		{
+			var obj = _db.Categories.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+
+			_db.Categories.Remove(obj);
+			_db.SaveChanges();
+			TempData["success"] = "Category deleted successfully";
+			return RedirectToAction("Index");
+
+		}
+	}
+}
